7d310632c02677ef7ba83f4b206d0e4d4f2ebf96
Philippe Gerhardt <philippe@sutoiku.com>
2025-03-11 16:19:32 +0100

    Fix hook import

--- src/application/mutations/use-create-note.ts
+++ src/application/mutations/use-create-note.ts
@@ -1,8 +1,6 @@
 import NotesService from "@/infrastructure/notes";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const queryClient = useQueryClient();
-
 interface CreateNoteInput {
   title: string;
   content: string;
@@ -14,6 +12,7 @@ interface CreateNoteInput {
 }
 
 export function useCreateNote() {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (input: CreateNoteInput) => NotesService.createNote(input),
     onSuccess: () => {
