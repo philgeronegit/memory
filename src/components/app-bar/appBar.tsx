749961dc57da95c7557b43bb5dadc0017bb01356
Philippe Gerhardt <philippe@sutoiku.com>
2025-03-13 10:45:06 +0100

    Add AppBar component

--- src/components/app-bar/appBar.tsx
+++ src/components/app-bar/appBar.tsx
@@ -1,9 +1,29 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarTrigger
+} from "@/components/ui/menubar";
+import { Menu } from "lucide-react";
+import Image from "next/image";
+import Logo from "../../assets/logo.png";
+
 const AppBar = () => {
   return (
     <header className="flex h-16 w-full items-center justify-between bg-background px-4 md:px-6">
-      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-        Memory
-      </h3>
+      <Image src={Logo} alt="Memory" width={64} height={64} />
+
+      <Menubar>
+        <MenubarMenu>
+          <MenubarTrigger>
+            <Menu />
+          </MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>Logout</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
     </header>
   );
 };
