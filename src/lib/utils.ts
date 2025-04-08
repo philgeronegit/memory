214ee3922b94d6f012bb6a7195bbb737aecb1331
Philippe Gerhardt <philippe@sutoiku.com>
2025-04-08 16:35:19 +0200

    Update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function toSnakeCase(str: string) {
+  return str
+    .split(" ")
+    .map((word) => word)
+    .join("_")
+    .replace(/[A-Z]/g, (letter) => `${letter.toLowerCase()}`);
+}
