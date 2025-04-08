176727ff0cc936e4b348adbad520c6ea69375d3e
Philippe Gerhardt <philippe@sutoiku.com>
2025-04-08 16:33:30 +0200

    Add inteceptors to axios

--- src/infrastructure/client.ts
+++ src/infrastructure/client.ts
@@ -1,6 +1,18 @@
 import axios from "axios";
 
-// const BASE_URL = "https://gerhardt.needemand.com/memory";
 const BASE_URL = "http://localhost/memory";
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 500) {
+      console.error("Server error:", error);
+      // Handle the error, e.g., display a message to the user
+      // or retry the request (with caution to avoid infinite loops)
+      return Promise.reject(error); // Propagate the error
+    }
+    return Promise.reject(error); // If not a 500 error, still reject
+  }
+);
 
 export const apiClient = axios.create({ baseURL: BASE_URL });
