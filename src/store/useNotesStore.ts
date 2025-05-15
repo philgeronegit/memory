7fc4ee6ac8335911b3888b6090299fd3a6dc2fa6
Philippe Gerhardt <philippe@sutoiku.com>
2025-05-15 12:26:35 +0200

    Update zustand state

--- src/store/useNotesStore.ts
+++ src/store/useNotesStore.ts
@@ -1,15 +1,25 @@
+import { User } from "@/domain/user";
+import { RoleUser } from "@/lib/auth";
 import { create } from "zustand";
 
 export interface NotesState {
+  user?: User;
+  roleUser?: RoleUser;
   noteContent?: string;
   selectedNoteId?: number;
+  setUser: (user: User | undefined) => void;
+  setRoleUser: (roleUser: RoleUser | undefined) => void;
   setNoteContent: (noteContent: string) => void;
   setSelectedNoteId: (id: number) => void;
 }
 
 const useNotesStore = create<NotesState>((set) => ({
+  user: undefined,
+  roleUser: undefined,
   noteContent: undefined,
   selectedNoteId: undefined,
+  setUser: (user) => set({ user }),
+  setRoleUser: (roleUser) => set({ roleUser }),
   setNoteContent: (noteContent) => set({ noteContent }),
   setSelectedNoteId: (id: number) => set({ selectedNoteId: id })
 }));
