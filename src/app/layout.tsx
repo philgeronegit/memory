1ff24ae8c0caafced03807b88c86f70d282a8ca2
Philippe Gerhardt <philippe@sutoiku.com>
2025-05-15 12:13:35 +0200

    Fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col h-screen`}>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col h-[96vh]`}>
         <Providers>
           <AppBar />
           {children}
