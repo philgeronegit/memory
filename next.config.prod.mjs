fdb50f71d5130f9952d0ff29b368dcba53f90ddc
Philippe Gerhardt <philippe@sutoiku.com>
2025-06-10 10:33:28 +0200

    Uncomment next prod config lines

--- next.config.prod.mjs
+++ next.config.prod.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export",
+  output: "export",
 
   // Ensures routing works under /memory
-  // basePath: "/memory",
+  basePath: "/memory",
   // Tells Next.js to generate correct URLs like /memory/_next/static/...
-  // assetPrefix: "/memory/",
+  assetPrefix: "/memory/",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+  trailingSlash: true,
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
+  skipTrailingSlashRedirect: true,
 
   // Optional: Change the output directory `out` -> `build`
   distDir: "build"
