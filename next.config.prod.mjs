743f1582af7d9d8b393fdb15531633b419fd4c25
Philippe Gerhardt <philippe@sutoiku.com>
2025-06-10 10:31:25 +0200

    Add dev and prod next config files

---
+++ next.config.prod.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // output: "export",
+
+  // Ensures routing works under /memory
+  // basePath: "/memory",
+  // Tells Next.js to generate correct URLs like /memory/_next/static/...
+  // assetPrefix: "/memory/",
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `build`
+  distDir: "build"
+};
+
+export default nextConfig;
