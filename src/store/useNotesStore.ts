c3bd62262dd2013dd9a64d09c130321d76012cfc
Philippe Gerhardt <philippe@sutoiku.com>
2025-06-11 09:41:48 +0200

    Add persistence to store

--- src/store/useNotesStore.ts
+++ src/store/useNotesStore.ts
@@ -1,6 +1,7 @@
 import { User } from "@/domain/user";
 import { RoleUser } from "@/lib/auth";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 export interface NotesState {
   user?: User;
@@ -13,15 +14,24 @@ export interface NotesState {
   setSelectedNoteId: (id: number) => void;
 }
 
-const useNotesStore = create<NotesState>((set) => ({
-  user: undefined,
-  roleUser: undefined,
-  noteContent: undefined,
-  selectedNoteId: undefined,
-  setUser: (user) => set({ user }),
-  setRoleUser: (roleUser) => set({ roleUser }),
-  setNoteContent: (noteContent) => set({ noteContent }),
-  setSelectedNoteId: (id: number) => set({ selectedNoteId: id })
-}));
+const useNotesStore = create<NotesState>()(
+  persist(
+    (set) => ({
+      user: undefined,
+      roleUser: undefined,
+      noteContent: undefined,
+      selectedNoteId: undefined,
+      setUser: (user) => set({ user }),
+      setRoleUser: (roleUser) => set({ roleUser }),
+      setNoteContent: (noteContent) => set({ noteContent }),
+      setSelectedNoteId: (id: number) => set({ selectedNoteId: id })
+    }),
+    {
+      name: "notes-storage",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({ selectedNoteId: state.selectedNoteId })
+    }
+  )
+);
 
 export default useNotesStore;
