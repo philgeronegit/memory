b4dec0f56969e3c37e2d483d2c7366659c4506a2
Philippe Gerhardt <philippe@sutoiku.com>
2025-06-12 15:49:15 +0200

    Add messages and update for small screens

--- src/components/app-bar/app-bar.tsx
+++ src/components/app-bar/app-bar.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { useLogin } from "@/application/mutations/use-login";
+import { useUserMessages } from "@/application/queries/use-user-messages";
+import { OptionsDialog } from "@/components/options";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import {
   Menubar,
   MenubarContent,
@@ -12,13 +22,11 @@ import {
 import { useToast } from "@/hooks/use-toast";
 import { hasPermission, Roles } from "@/lib/auth";
 import useNotesStore from "@/store/useNotesStore";
-import { LogIn } from "lucide-react";
-import Image from "next/image";
+import { Ellipsis, LogIn, Mail } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 import { useEffectOnce, useMedia } from "react-use";
-import Logo from "../../assets/logo-64.png";
 import { Button } from "../ui/button";
 import { LoginDialog } from "../users";
 
@@ -40,9 +48,21 @@ const CustomLink = ({ label, pathname, path = "/" }: CustomLinkProps) => (
   </Link>
 );
 
+const links = [
+  { label: "Home", path: "/" },
+  { label: "Dashboard", path: "/dashboard" },
+  { label: "Profile", path: "/profile" },
+  { label: "Messages", path: "/messages" },
+  { label: "Uploads", path: "/uploads" },
+  { label: "Kanban", path: "/kanban" },
+  { label: "Utilisateurs", path: "/users" }
+];
+
 export const AppBar = () => {
   const pathname = usePathname();
   const [open, setOpen] = useState(false);
+  const [isOptionsDialogOpen, setOptionsDialogOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const { user, roleUser } = useNotesStore();
   const avatarUrl = user?.avatarUrl;
   const username = user?.username;
@@ -57,6 +77,12 @@ export const AppBar = () => {
   const router = useRouter();
   const { toast } = useToast();
   const isWide = useMedia("(min-width: 480px)");
+  const { data: userMessages, isLoading: isLoadingMessages } = useUserMessages({
+    userId: user?.id
+  });
+  const hasUnreadMessages =
+    userMessages && userMessages.some((message) => !message.readAt);
+  const hasMessages = userMessages && userMessages.length > 0;
 
   useEffectOnce(() => {
     const runAsync = async () => {
@@ -86,7 +112,7 @@ export const AppBar = () => {
     if (itemName === "modify-pasword") {
       router.push("/profile/change-password");
     } else if (itemName === "settings") {
-      router.push("/profile/settings");
+      setOptionsDialogOpen(true);
     } else if (itemName === "logout") {
       setUser(undefined);
       setRoleUser(undefined);
@@ -96,9 +122,36 @@ export const AppBar = () => {
 
   return (
     <header className="flex h-16 w-full items-center justify-between border border-slate-300 bg-background px-4 md:px-6">
-      <Image src={Logo} alt="Memory" />
-
-      {user && (
+      <div className="flex items-center gap-2">
+        {!isWide && (
+          <DropdownMenu open={dropdownOpen} onOpenChange={setDropdownOpen}>
+            <DropdownMenuTrigger>
+              <Button variant="ghost" className="p-2">
+                <Ellipsis />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Menu</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              {links.map((link) => (
+                <DropdownMenuItem
+                  key={link.path}
+                  onSelect={() => setDropdownOpen(false)}>
+                  <CustomLink
+                    pathname={pathname}
+                    path={link.path}
+                    label={link.label}
+                  />
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+        <h4 className="text-primary scroll-m-20 text-xl font-semibold tracking-tight">
+          Memory
+        </h4>
+      </div>
+      {user && isWide && (
         <div className="flex items-center gap-4">
           <CustomLink pathname={pathname} path={"/"} label="Home" />
           <CustomLink
@@ -107,6 +160,8 @@ export const AppBar = () => {
             label="Dashboard"
           />
           <CustomLink pathname={pathname} path={"/profile"} label="Profile" />
+          <CustomLink pathname={pathname} path={"/messages"} label="Messages" />
+          <CustomLink pathname={pathname} path={"/uploads"} label="Uploads" />
           {isWide && (
             <CustomLink pathname={pathname} path={"/kanban"} label="Kanban" />
           )}
@@ -119,7 +174,6 @@ export const AppBar = () => {
           ) : null}
         </div>
       )}
-
       {!user && (
         <div className="flex items-center gap-4">
           <Button variant="ghost" title="Login" onClick={() => setOpen(true)}>
@@ -127,9 +181,23 @@ export const AppBar = () => {
           </Button>
         </div>
       )}
-
       {user && (
         <div className="flex gap-2">
+          {isLoadingMessages && <Ellipsis className="animate-spin" />}{" "}
+          {hasMessages && (
+            <Link
+              href="/messages"
+              className="relative flex items-center justify-center">
+              {hasUnreadMessages && (
+                <>
+                  <span className="absolute -top-1 -right-1 inline-flex h-2.5 w-2.5 animate-ping rounded-full bg-red-400 opacity-75"></span>
+                  <span className="absolute -top-1 -right-1 inline-flex h-2.5 w-2.5 rounded-full bg-red-500"></span>
+                  <span className="sr-only">Messages</span>
+                </>
+              )}
+              <Mail />
+            </Link>
+          )}
           <Menubar className="border-0 shadow-none">
             <MenubarMenu>
               <MenubarTrigger>
@@ -165,8 +233,11 @@ export const AppBar = () => {
           </Menubar>
         </div>
       )}
-
       <LoginDialog open={open} setOpen={setOpen} />
+      <OptionsDialog
+        isOpen={isOptionsDialogOpen}
+        onClose={() => setOptionsDialogOpen(false)}
+      />
     </header>
   );
 };
