5bfe0e928dda81642e61ac1c1a1914d6fd390ae2
Philippe Gerhardt <philippe@sutoiku.com>
2025-12-30 12:10:59 +0100

    Refactor Login component to improve structure and handle redirect logic based on search parameters

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,30 +3,53 @@
 import { Button } from "@/components/ui/button";
 import { LoginDialog } from "@/components/users";
 import useNotesStore from "@/store/useNotesStore";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense, useEffect, useState } from "react";
+
+function LoginContent() {
+	const [open, setOpen] = useState(false);
+	const { user } = useNotesStore();
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const [logging, setLogging] = useState(false);
+	const redirectUrl = searchParams.get("redirect") || "/";
+	console.log("🚀 ~ Login ~ redirectUrl:", redirectUrl);
+
+	useEffect(() => {
+		if (user) {
+			router.push(redirectUrl);
+		}
+	}, [user, router, redirectUrl]);
+
+	const handleLogging = () => {
+		setLogging(true);
+		setOpen(true);
+	};
+
+	return (
+		<div>
+			{!logging && (
+				<div className="p-4 text-center">
+					Vous devez vous connecter pour utiliser cette application
+				</div>
+			)}
+			<div className="flex justify-center p-2">
+				{logging ? (
+					<div>En cours de connexion...</div>
+				) : (
+					<Button onClick={handleLogging}>Se connecter</Button>
+				)}
+			</div>
+
+			<LoginDialog open={open} setOpen={setOpen} />
+		</div>
+	);
+}
 
 export default function Login() {
-  const [open, setOpen] = useState(false);
-  const { user } = useNotesStore();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user) {
-      router.push("/");
-    }
-  }, [user, router]);
-
-  return (
-    <div>
-      <div className="p-4 text-center">
-        Vous devez vous connecter pour utiliser cette application
-      </div>
-      <div className="flex justify-center p-2">
-        <Button onClick={() => setOpen(true)}>Login</Button>
-      </div>
-
-      <LoginDialog open={open} setOpen={setOpen} />
-    </div>
-  );
+	return (
+		<Suspense fallback={<div className="p-4 text-center">Chargement...</div>}>
+			<LoginContent />
+		</Suspense>
+	);
 }
