f3230fe892a2975e1aa60fa7df15f7db3a80450a
Philippe Gerhardt <philippe@sutoiku.com>
2025-12-31 19:03:59 +0100

    Wrap uploads component in AuthWrapper for improved authentication handling

--- src/app/uploads/page.tsx
+++ src/app/uploads/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useUserUploads } from "@/application/queries/use-user-uploads";
+import { AuthWrapper } from '@/components/auth';
 import {
   Popover,
   PopoverContent,
@@ -47,57 +48,59 @@ export default function Uploads() {
   }
 
   return (
-    <div className="p-2">
-      <FileUpload />
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Nom</TableHead>
-            <TableHead>Chemin</TableHead>
-            <TableHead>Thumbnail</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {uploads?.map((upload) => (
-            <TableRow key={upload.path}>
-              <TableCell className="w-[100px]">{upload.name}</TableCell>
-              <TableCell>{upload.path}</TableCell>
-              <TableCell>
-                <Popover
-                  open={openPopover === upload.path}
-                  onOpenChange={(open) =>
-                    setOpenPopover(open ? upload.path : null)
-                  }>
-                  <PopoverTrigger asChild>
-                    <img
-                      src={`${BASE_URL}/${upload.path}`}
-                      alt={upload.name}
-                      width={64}
-                      height={64}
-                      className="ml-2 inline-block rounded shadow hover:ring-2 hover:ring-primary transition"
-                      onMouseEnter={() => setOpenPopover(upload.path)}
-                      onMouseLeave={() => setOpenPopover(null)}
-                    />
-                  </PopoverTrigger>
-                  <PopoverContent
-                    align="center"
-                    className="flex items-center justify-center w-auto h-auto bg-white p-2"
-                    onMouseEnter={() => setOpenPopover(upload.path)}
-                    onMouseLeave={() => setOpenPopover(null)}>
-                    <img
-                      src={`${BASE_URL}/${upload.path}`}
-                      alt={upload.name}
-                      width={256}
-                      height={256}
-                      className="rounded shadow-lg"
-                    />
-                  </PopoverContent>
-                </Popover>
-              </TableCell>
+    <AuthWrapper>
+      <div className="p-2">
+        <FileUpload />
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Nom</TableHead>
+              <TableHead>Chemin</TableHead>
+              <TableHead>Thumbnail</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+          </TableHeader>
+          <TableBody>
+            {uploads?.map((upload) => (
+              <TableRow key={upload.path}>
+                <TableCell className="w-[100px]">{upload.name}</TableCell>
+                <TableCell>{upload.path}</TableCell>
+                <TableCell>
+                  <Popover
+                    open={openPopover === upload.path}
+                    onOpenChange={(open) =>
+                      setOpenPopover(open ? upload.path : null)
+                    }>
+                    <PopoverTrigger asChild>
+                      <img
+                        src={`${BASE_URL}/${upload.path}`}
+                        alt={upload.name}
+                        width={64}
+                        height={64}
+                        className="ml-2 inline-block rounded shadow hover:ring-2 hover:ring-primary transition"
+                        onMouseEnter={() => setOpenPopover(upload.path)}
+                        onMouseLeave={() => setOpenPopover(null)}
+                      />
+                    </PopoverTrigger>
+                    <PopoverContent
+                      align="center"
+                      className="flex items-center justify-center w-auto h-auto bg-white p-2"
+                      onMouseEnter={() => setOpenPopover(upload.path)}
+                      onMouseLeave={() => setOpenPopover(null)}>
+                      <img
+                        src={`${BASE_URL}/${upload.path}`}
+                        alt={upload.name}
+                        width={256}
+                        height={256}
+                        className="rounded shadow-lg"
+                      />
+                    </PopoverContent>
+                  </Popover>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </AuthWrapper>
   );
 }
