ebe47c6cc3b3c308d6cbb7aaf8c6ff00879fe62c
Philippe Gerhardt <philippe@sutoiku.com>
2026-01-05 12:11:02 +0100

    Refactor layout to use local font variables and apply list styles to HTML elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import { AppBar } from "@/components/app-bar";
 import { Toaster } from "@/components/ui/toaster";
-import { GeistMono } from "geist/font/mono";
-import { GeistSans } from "geist/font/sans";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -29,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} list-inside list-disc`}>
       <body className="antialiased flex flex-col h-[96vh]">
         <Providers>
           <AppBar />
